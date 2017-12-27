36238581fbd8483ffc15caf1e5b7085c437642e9
Edith <edith.lopezhuerta@gmail.com>
2017-12-26 19:12:47 -0600

    Ya esta los ejercicios hamburguesa y tabs

--- tabs/app.js
+++ tabs/app.js
@@ -1,20 +1,37 @@
 var mostrarOcultar = function (e){
   var tabSeleccionado = e.target.dataset.tabSeleccionado;
+  var desayuno = document.getElementById("desayuno");
+  var comida = document.getElementById("comida");
+  var postres = document.getElementById("postres");
+
   if(tabSeleccionado === "tabdesayuno") {
-    console.log("vamos a desayunar");
-    /*ocultar comida y postres
-    //mostrar desayuno*/
+    /*ocultar comida y postres*/
+    comida.style.display = "none";
+    postres.style.display = "none"
+    /*mostrar desayuno*/
+    desayuno.style.display = "block";
   }else if (tabSeleccionado === "tabcomida"){
-    console.log("vamos a comer");
-    /*ocultar desayuno y postres
-    //mostrarcomida*/
+    /*ocultar desayuno y postres*/
+    desayuno.style.display = "none";
+    postres.style.display = "none"
+    /*mostrar comida*/
+    comida.style.display = "block";
   }else if(tabSeleccionado === "tabpostres"){
-    console.log("vamos a comer un postre");
-    /*ocultar desayuno y Comida
-    mostrar postres*/
+    /*ocultar desayuno y comida*/
+    desayuno.style.display = "none";
+    comida.style.display = "none"
+    /*mostrar postres*/
+    postres.style.display = "block";
   }
 }
 var cargarPagina = function() {
+  /*ocultar la opción menú al cargar la página */
+  var desayuno = document.getElementById("desayuno");
+  var comida = document.getElementById("comida");
+  var postres = document.getElementById("postres");
+  desayuno.style.display = "none";
+  comida.style.display = "none"
+  postres.style.display = "none";
    var elementosTabs = document.getElementsByClassName('tab');
    for (var i = 0; i<elementosTabs.length; i++) {
       elementosTabs[i].addEventListener("click", mostrarOcultar);
